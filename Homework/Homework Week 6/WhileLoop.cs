0e69ce2b601d1e9a19c10e1060eac057c2a8d740
willhuestis <93241578+willhuestis@users.noreply.github.com>
2021-11-08 20:41:07 -0800

    Create WhileLoop.cs

---
+++ Homework/Homework Week 6/WhileLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoopInsideALoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a word.");
+            string newWord = Console.ReadLine();
+            string currentWinner =//need to find a way to define currentWinner here so it doesnt break on loop ;
+            while (true) ;          
+            {
+                if (newWord.Length > currentWinner.length);
+                {
+                    currentWinner = Console.ReadLine();
+                }
+            }
+
+
+        }
+    }
+}
